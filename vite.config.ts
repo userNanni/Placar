40b47117a0784eedbb75919ecab320ad7776a229
userNanni <121840951+userNanni@users.noreply.github.com>
2023-05-18 14:54:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `https://usernanni.github.io/Placar/`,
+  base: "/Placar/",
   plugins: [react()],
 });
