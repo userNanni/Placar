efa8bb7dca0bc8d308b4a9d3e2a5d8c026b28bf7
userNanni <usuarionanni@gmail.com>
2024-05-12 10:35:54 -0300

    0.1.1

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -73,14 +73,14 @@ function Home() {
           alt=""
         />
         <div className="container">
-          <a href="">
+          <a href="https://www.youtube.com/@AFAMilitar">
             <img
               src="https://github.com/userNanni/Placar/blob/10f62f35e067a4b4558087a1034ce00cbbe5c436/src/assets/youtube.png?raw=true"
               width={60}
               alt=""
             />
           </a>
-          <a href="">
+          <a href="https://www.instagram.com/afamilitar/">
             <img
               src="https://github.com/userNanni/Placar/blob/10f62f35e067a4b4558087a1034ce00cbbe5c436/src/assets/instagram.png?raw=true"
               width={60}
